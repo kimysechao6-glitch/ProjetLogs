e5efa723054ee899471da056e25235e27cb960b5
kimy <kimysechao@gmail.com>
2026-09-24 17:09:16 +0200

    Remove unnecessary includes and logging from logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,5 @@
 <?php
 require_once __DIR__ . '/helpers.php';
-include 'helpers.php';
-include 'logger.php';
 session_destroy();
 header('Location: /');
-$user = current_user()['username'];
-write_log ("INFO", $user, "Déconnexion du compte", "SUCCES");
 exit;
