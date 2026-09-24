e379c7e50161ff4014537a17893ae1485d846402
Nwt <mailletnills@gmail.com>
2026-09-24 16:44:47 +0200

    Include logging and helper files in logout.php
    Added logging for user logout activity.

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 require_once __DIR__ . '/helpers.php';
+include 'helpers.php';
+include 'logger.php';
 session_destroy();
 header('Location: /');
+write_log ("INFO", current_user()['username'], "Déconnexion du compte", "SUCCES");
 exit;
